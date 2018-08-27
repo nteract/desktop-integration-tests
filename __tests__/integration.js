b8a49b2cb96c91e6ee8212a668c5f0a2d0dcc2f0
alexandercbooth <alexander.c.booth@gmail.com>
2018-08-26 23:35:23 -0400

    adding integration test

---
+++ __tests__/integration.js
@@ -0,0 +1,59 @@
+const Application = require("spectron").Application;
+const assert = require("assert");
+const path = require("path");
+const appPath = "nteract/applications/desktop/dist/squashfs-root";
+const clearNotebookPath = "notebooks/python-example.ipynb";
+const executedNotebookPath = "notebooks/python-example-executed.ipynb";
+
+const delay = time => new Promise(resolve => setTimeout(resolve, time));
+
+jest.setTimeout(15000);
+
+describe("Testing notebook actions in python with clean notebook", () => {
+  beforeEach(() => {
+    app = new Application({
+      path: appPath,
+      args: [clearNotebookPath]
+    });
+    return app.start();
+  });
+
+  afterEach(async () => {
+    if (app && app.isRunning()) {
+      // app.stop();
+      await delay(2000);
+      return app.browserWindow.destroy();
+    }
+  });
+
+  it("Open python example, clears all cells", async () => {
+    await delay(12000);
+    await app.client.windowByIndex(0);
+    await app.browserWindow.send("menu:clear-all");
+    await app.browserWindow.send("menu:run-all");
+  });
+});
+
+describe("Testing notebook actions in python with dirty notebook", () => {
+  beforeEach(() => {
+    app = new Application({
+      path: appPath,
+      args: [executedNotebookPath]
+    });
+    return app.start();
+  });
+
+  afterEach(async () => {
+    if (app && app.isRunning()) {
+      // app.stop();
+      await delay(2000);
+      return app.browserWindow.destroy();
+    }
+  });
+
+  it("Open python example, clears all cells", async () => {
+    await delay(12000);
+    await app.client.windowByIndex(0);
+    await app.browserWindow.send("menu:clear-all");
+  });
+});
