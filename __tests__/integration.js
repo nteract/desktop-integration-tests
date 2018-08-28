f27cb57b949c0685040e47caba8bb33faeaddf71
alexandercbooth <alexander.c.booth@gmail.com>
2018-08-28 16:20:18 -0400

    letting original notebooks be immutable, gracefully closing notebooks after each test and taking snapshots of executed notebooks :smile:

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -1,60 +1,63 @@
 const Application = require("spectron").Application;
 const assert = require("assert");
 const path = require("path");
-const appPath = "nteract/applications/desktop/dist/squashfs-root";
+const fs = require("fs");
+const appPath = "nteract/applications/desktop/dist/squashfs-root/nteract";
 
-const clearNotebookPath = "notebooks/node-example.ipynb";
+const cleanNotebookPath = "notebooks/node-example.ipynb";
 const executedNotebookPath = "notebooks/node-example-executed.ipynb";
 
 const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
 jest.setTimeout(15000);
 
-describe("Testing notebook actions in python with clean notebook", () => {
+let i = 0;
+let nextTestNb = executedNotebookPath;
+describe("Testing notebook actions in python with dirty notebook", () => {
   beforeEach(() => {
+    i += 1;
+    const newNotebook = `notebooks/notebook${i}.ipynb`;
+    fs.createReadStream(nextTestNb).pipe(fs.createWriteStream(newNotebook));
     app = new Application({
       path: appPath,
-      args: [clearNotebookPath]
+      args: [newNotebook]
     });
     return app.start();
   });
 
   afterEach(async () => {
     if (app && app.isRunning()) {
-      // app.stop();
       await delay(2000);
-      return app.browserWindow.destroy();
+      return app.stop();
     }
   });
 
-  it("Open python example, clears all cells", async () => {
+  it("Open dirty notebook example, clears all cells", async () => {
     await delay(12000);
     await app.client.windowByIndex(0);
     await app.browserWindow.send("menu:clear-all");
+    await app.browserWindow.send("menu:save");
+    nextTestNb = cleanNotebookPath;
+  });
+
+  it("Open clean notebook example, runs all cells", async () => {
+    await delay(12000);
+    await app.client.windowByIndex(0);
     await app.browserWindow.send("menu:run-all");
+    await app.browserWindow.send("menu:save");
   });
 });
 
-describe("Testing notebook actions in python with dirty notebook", () => {
-  beforeEach(() => {
-    app = new Application({
-      path: appPath,
-      args: [executedNotebookPath]
-    });
-    return app.start();
+describe("tests jest snapshots of executed notebooks", () => {
+  it("tests clear all-cells", () => {
+    const nbPath = `notebooks/notebook1.ipynb`;
+    const nb = JSON.parse(fs.readFileSync(nbPath));
+    expect(nb).toMatchSnapshot();
   });
 
-  afterEach(async () => {
-    if (app && app.isRunning()) {
-      // app.stop();
-      await delay(2000);
-      return app.browserWindow.destroy();
-    }
-  });
-
-  it("Open python example, clears all cells", async () => {
-    await delay(12000);
-    await app.client.windowByIndex(0);
-    await app.browserWindow.send("menu:clear-all");
+  it("tests run-all-cells", () => {
+    const nbPath = `notebooks/notebook2.ipynb`;
+    const nb = JSON.parse(fs.readFileSync(nbPath));
+    expect(nb).toMatchSnapshot();
   });
 });
