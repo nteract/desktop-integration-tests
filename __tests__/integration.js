df60e4a794838b8d09ced3d1609d62813a44cba9
alexandercbooth <alexander.c.booth@gmail.com>
2018-09-02 22:23:38 -0400

    add tests for new code and new text cells

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -3,9 +3,13 @@ const assert = require("assert");
 const path = require("path");
 const fs = require("fs");
 
-const appPath = "nteract/applications/desktop/dist/squashfs-root/nteract";
+// const appPath = "nteract/applications/desktop/dist/squashfs-root/nteract";
+const appPath =
+  "/Users/acb/contributing/tt/nteract/applications/desktop/dist/mac/nteract.app/Contents/MacOS/nteract";
 const cleanNotebookPath = "notebooks/node-example.ipynb";
 const executedNotebookPath = "notebooks/node-example-executed.ipynb";
+const newCodeCellNotebookPath = "notebooks/newCodeCell-example.ipynb";
+const newTextCellNotebookPath = "notebooks/newTextCell-example.ipynb";
 
 const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
@@ -42,6 +46,29 @@ describe("Testing notebook actions in python with dirty notebook", () => {
     await app.client.windowByIndex(0);
     await app.browserWindow.send("menu:run-all");
     await app.browserWindow.send("menu:save-as", "notebooks/run-all.ipynb");
+    nextTestNb = newCodeCellNotebookPath;
+  });
+
+  it("Open notebook and adds a new code cell", async () => {
+    await delay(12000);
+    await app.client.windowByIndex(0);
+    await app.browserWindow.send("menu:new-code-cell");
+    await app.browserWindow.send(
+      "menu:save-as",
+      "notebooks/new-code-cell.ipynb"
+    );
+    nextTestNb = newTextCellNotebookPath;
+  });
+
+  it("Open notebook and adds a new text cell", async () => {
+    await delay(12000);
+    await app.client.windowByIndex(0);
+    await app.browserWindow.send("menu:new-text-cell");
+    await app.browserWindow.send(
+      "menu:save-as",
+      "notebooks/new-text-cell.ipynb"
+    );
+    // nextTestNb = newTextCellNotebookPath;
   });
 });
 
@@ -54,6 +81,18 @@ describe("tests jest snapshots of executed notebooks", () => {
 
   it("tests run-all-cells", () => {
     const nbPath = "notebooks/run-all.ipynb";
+    const nb = JSON.parse(fs.readFileSync(nbPath));
+    expect(nb).toMatchSnapshot();
+  });
+
+  it("tests new-code-cells", () => {
+    const nbPath = "notebooks/new-code-cell.ipynb";
+    const nb = JSON.parse(fs.readFileSync(nbPath));
+    expect(nb).toMatchSnapshot();
+  });
+
+  it("tests new-text-cells", () => {
+    const nbPath = "notebooks/new-text-cell.ipynb";
     const nb = JSON.parse(fs.readFileSync(nbPath));
     expect(nb).toMatchSnapshot();
   });
